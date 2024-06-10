21e863740fa839e951749f83e180ba91d33ef9f2
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-10 13:18:42 +0900

    :sparkles: int 타입 변수에 각각의 진수를 입력하고 10진수로 출력

---
+++ Grammer/Variable/Type/Integer/IntegerListExample.java
@@ -0,0 +1,17 @@
+package Grammer.Variable.Type.Integer;
+
+import Grammer.Variable.InitialExample;
+
+public class IntegerListExample {
+    public static void main(String[] args) {
+        int var1 = 0b1001; //2진수
+        int var2 = 0206; //8진수
+        int var3 = 365; //10진수
+        int var4 = 0xB3; //16진수
+
+        System.out.println("var1 : " + var1);
+        System.out.println("var2 : " + var2);
+        System.out.println("var3 : " + var3);
+        System.out.println("var4 : " + var4);
+    }
+}
