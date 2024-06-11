d4a730de3a67f7417bd0916fb300f078913ac0d3
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-11 16:35:10 +0900

    :sparkles: 1차원 배열

--- Grammer/Arrary/ArrayExample.java
+++ Grammer/Arrary/ArrayExample.java
@@ -1,2 +1,17 @@
-package Grammer.Arrary;public class ArrayExample {
+package Grammer.Arrary;
+
+import java.util.Arrays;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] iArr = {100, 95, 80, 70, 60};
+
+        // 루프문으로 직접 배열 원소 출력
+        for(int i = 0 ; i < iArr.length; i++){
+            System.out.println(iArr[i]);
+        }
+
+        // Arrays.toString() 메서드 사용하여 심플하게 바로 출력
+        System.out.println(Arrays.toString(iArr)); // [100, 95, 80, 70, 60]
+    }
 }
