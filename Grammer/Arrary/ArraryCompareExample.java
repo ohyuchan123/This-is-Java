e807c2ce2e68448d83175359f19c1cc970ee09d0
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-11 16:35:10 +0900

    :sparkles: 1차원 배열 비(equals)

--- Grammer/Arrary/ArraryCompareExample.java
+++ Grammer/Arrary/ArraryCompareExample.java
@@ -1,2 +1,15 @@
-package Grammer.Arrary;public class ArraryCompareExample {
+package Grammer.Arrary;
+
+import java.util.Arrays;
+
+public class ArraryCompareExample {
+    public static void main(String[] args) {
+        String[] arr1 = { "홍길동", "임꺽정", "박혁거세", "주몽", "고담덕" };
+        String[] arr2 = { "홍길동", "임꺽정", "박혁거세", "주몽", "고담덕" };
+        String[] arr3 = { "홍길동", "임꺽정", "박혁거세", "주몽" };
+
+        System.out.println("arr1 == arr2 : " + Arrays.equals(arr1, arr2)); // arr1 == arr2 : true
+
+        System.out.println("arr1 == arr3 : " + Arrays.equals(arr1, arr3)); // arr1 == arr3 : false
+    }
 }
