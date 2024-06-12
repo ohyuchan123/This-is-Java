17ff2d7952f1886bbdbf91f94d759e508326b6ea
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-12 14:35:10 +0900

    :sparkles: 클래스 오버로딩

--- Grammer/Class/Constructor/Overloading/Person.java
+++ Grammer/Class/Constructor/Overloading/Person.java
@@ -1,2 +1,27 @@
-package Grammer.Class.Constructor.Overloading;public class Person {
+package Grammer.Class.Constructor.Overloading;
+
+public class Person {
+    private String name;
+    private int age;
+
+    // 기본 생성자
+    public Person() {
+        this("Unknown", 0);
+    }
+
+    // 이름만 초기화하는 생성자
+    public Person(String name) {
+        this(name, 0);
+    }
+
+    // 이름과 나이를 초기화하는 생성자
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("이름: " + name + ", 나이: " + age);
+    }
+
 }
--- Grammer/Class/Constructor/Overloading/PersonExample.java
+++ Grammer/Class/Constructor/Overloading/PersonExample.java
@@ -1,2 +1,14 @@
-package Grammer.Class.Constructor.Overloading;public class PersonExample {
+package Grammer.Class.Constructor.Overloading;
+
+public class PersonExample {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.displayInfo();
+
+        Person person2 = new Person("Alice");
+        person2.displayInfo();
+
+        Person person3 = new Person("Bob", 25);
+        person3.displayInfo();
+    }
 }
