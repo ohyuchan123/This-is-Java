3ee202116a575bed1342f67b3402a6c01116957e
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-12 14:35:10 +0900

    :sparkles: 필드

--- Grammer/Class/Fields/Field.java
+++ Grammer/Class/Fields/Field.java
@@ -1,2 +1,11 @@
-package Grammer.Class.Field;public class Field {
+package Grammer.Class.Fields;
+
+public class Field {
+    static int classVar = 10; // 클래스/스태틱 변수 선언
+    int instanceVar = 20;     // 인스턴스 변수 선언
+
+    int method() {
+        int localVar = 30; // 지역 변수 선언
+        return localVar;
+    }
 }
--- Grammer/Class/Fields/Member01.java
+++ Grammer/Class/Fields/Member01.java
@@ -1,2 +1,13 @@
-package Grammer.Class.Fields;public class Member01 {
+package Grammer.Class.Fields;
+
+public class Member01 {
+    public static void main(String[] args) {
+        System.out.println( Field.classVar ); // 클래스/스태틱 변수 참조
+
+        Field myField1 = new Field();   // 인스턴스 생성
+
+        System.out.println( myField1.instanceVar ); // 인스턴스 변수 참조
+
+        System.out.println( myField1.method() ); // 메서드안의 지역변수 출력
+    }
 }
