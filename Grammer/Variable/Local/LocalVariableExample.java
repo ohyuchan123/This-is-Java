27145166b140a51b8ebfb2ff46a8484c065fcff0
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-12 14:35:10 +0900

    :sparkles: 지역 변수

--- Grammer/Variable/Local/LocalVariableExample.java
+++ Grammer/Variable/Local/LocalVariableExample.java
@@ -1,2 +1,15 @@
-package Grammer.Variable.Local;public class LocalVariableExample {
+package Grammer.Variable.Local;
+
+public class LocalVariableExample {
+    public void calculateSum() {
+        int num1 = 10; // 지역 변수 num1
+        int num2 = 20; // 지역 변수 num2
+        int sum = num1 + num2; // 지역 변수 sum
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void main(String[] args) {
+        LocalVariableExample example = new LocalVariableExample();
+        example.calculateSum();
+    }
 }
