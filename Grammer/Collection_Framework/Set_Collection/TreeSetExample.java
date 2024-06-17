99d3c64e5e6a82db73cbfb5740ddfd17fe449dc9
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-17 16:00:00 +0900

    :sparkles: TreeSet 클래스(정렬하여 저장)

--- Grammer/Collection_Framework/Set_Collection/TreeSetExample.java
+++ Grammer/Collection_Framework/Set_Collection/TreeSetExample.java
@@ -1,2 +1,18 @@
-package Grammer.Collection_Framework.Set_Collection;public class TreeSetExampel {
+package Grammer.Collection_Framework.Set_Collection;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        Set<Integer> treeSet = new TreeSet<>();
+
+        treeSet.add(7);
+        treeSet.add(4);
+        treeSet.add(9);
+        treeSet.add(1);
+        treeSet.add(5);
+
+        System.out.println(treeSet.toString()); // [1, 4, 5, 7, 9] - 자료가 알아서 정렬됨
+    }
 }
