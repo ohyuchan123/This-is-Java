03d54254f5f6745c01581d30e9760d21a8d0778a
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-18 16:00:00 +0900

    :sparkles: Lambda

--- Grammer/Lambda/Calculable.java
+++ Grammer/Lambda/Calculable.java
@@ -1,2 +1,7 @@
-package Grammer.Lambda;public interface Calculable {
+package Grammer.Lambda;
+
+@FunctionalInterface
+public interface Calculable {
+    // 추상 메소드
+    void calculable(int x,int y);
 }
--- Grammer/Lambda/LambdaExample.java
+++ Grammer/Lambda/LambdaExample.java
@@ -1,2 +1,23 @@
-package Grammer.Lambda;public class LambdaExample {
+package Grammer.Lambda;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        action((x,y)->{
+            int result = x + y;
+            System.out.println("result : "+result);
+        });
+
+        action((x,y)->{
+            int result = x - y;
+            System.out.println("result : "+result);
+        });
+    }
+
+    public static void action(Calculable calculable){
+        //데이터
+        int x = 10;
+        int y = 4;
+        //데이터 처리
+        calculable.calculable(x,y);
+    }
 }
