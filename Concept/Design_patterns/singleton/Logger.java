bffa50e8dac5e250be33ef573d3480c23d3d5428
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-19 16:00:00 +0900

    :sparkles: 싱글톤 패턴

--- Concept/Design_patterns/singleton/Logger.java
+++ Concept/Design_patterns/singleton/Logger.java
@@ -1,2 +1,32 @@
-package Concept.Design_patterns.singleton;public class Logger {
+package Concept.Design_patterns.singleton;
+
+import java.time.LocalDateTime;
+
+// app 전체에서 log를 담당하는 클래스
+// 이 클래스의 객체는 단 한개
+public class Logger {
+
+    // private static Logger 변수
+//    private static Logger logger = new Logger(); // 방법 : 1
+    private static Logger logger; //방법 : 2
+
+    // 생성자를 private로
+    private Logger(){ }
+
+    // static 메서드로 외부에서 객체를 얻을 수 있는 방법 제공
+    public static Logger getInstance(){
+        if(logger == null){
+            logger = new Logger();
+        }
+        return logger;
+    }
+
+    public void log(String message){
+        LocalDateTime ldt = LocalDateTime.now();
+        String date = ldt.getYear() + "/" + ldt.getMonthValue() + "/" +
+                ldt.getDayOfMonth();
+        String time = ldt.getHour() + ":" + ldt.getMinute() + ":" + ldt.getSecond();
+
+        System.out.println("["+date+" "+time+"] "+message);
+    }
 }
--- Concept/Design_patterns/singleton/Test.java
+++ Concept/Design_patterns/singleton/Test.java
@@ -1,2 +1,19 @@
-package Concept.Design_patterns.singleton;public class Test {
+package Concept.Design_patterns.singleton;
+
+public class Test {
+    public static void main(String[] args) {
+//        Logger logger = new Logger();
+//        Logger logger2 = new Logger();
+
+        Logger logger = Logger.getInstance();
+        System.out.println(logger);
+
+        m();
+    }
+
+    public static void m(){
+        Logger logger = Logger.getInstance();
+        System.out.println(logger);
+        logger.log("첫 번째 로그입니다.");
+    }
 }
